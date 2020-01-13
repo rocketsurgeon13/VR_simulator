e7895cbd6b042195381f853450e0bafc53d19df8
Laura L <lmlehtinen@gmail.com>
2020-01-13 15:23:28 +0200

    no message

--- VRGame/Assets/ResolutionLow.cs
+++ VRGame/Assets/ResolutionLow.cs
@@ -13,19 +13,7 @@ public class ResolutionLow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void OnGUI()
-    {
-        string[] names = QualitySettings.names;
-        GUILayout.BeginVertical();
-        for (int i = 0; i < names.Length; i++)
-        {
-            if (GUILayout.Button(names[i]))
-            {
-                QualitySettings.SetQualityLevel(i, true);
-            }
-        }
-        GUILayout.EndVertical();
-    }
+   
     void Update()
     {
         if (Input.GetKeyDown("x"))
