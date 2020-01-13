cc06950214e3a7b398a8c4c5057725d249113559
Laura L <lmlehtinen@gmail.com>
2020-01-13 13:46:11 +0200

    näköongelma valmis kiitos jeesus en jaksa enää väkertää

---
+++ VRGame/Assets/ResolutionLow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionLow : MonoBehaviour
+{
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        string[] names = QualitySettings.names;
+        GUILayout.BeginVertical();
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (GUILayout.Button(names[i]))
+            {
+                QualitySettings.SetQualityLevel(i, true);
+            }
+        }
+        GUILayout.EndVertical();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            QualitySettings.masterTextureLimit = 100;
+            
+
+        }
+        if (Input.GetKeyDown("z"))
+        {
+            QualitySettings.masterTextureLimit = 1;
+        }
+    }
+}
