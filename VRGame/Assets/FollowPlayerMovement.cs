11b0ba328f0cbdb468c44a7e8c2e51b047103f07
Laura L <lmlehtinen@gmail.com>
2020-01-15 11:04:42 +0200

    meikän skeneen juttui

--- VRGame/Assets/FollowPlayerMovement.cs
+++ VRGame/Assets/FollowPlayerMovement.cs
@@ -1,17 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FollowPlayerMovement : MonoBehaviour
-{
-
-    public GameObject Player;
-    public float movementSpeed = 4;
-    void Update()
-    {
-        transform.LookAt(Player.transform);
-    }
-}
-
-
-
+﻿
