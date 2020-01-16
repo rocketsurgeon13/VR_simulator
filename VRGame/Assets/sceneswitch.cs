32b6ef085e6c34b5e7c4777af85704b6cc8be8d7
Laura L <lmlehtinen@gmail.com>
2020-01-16 11:01:40 +0200

    all them shiz

--- VRGame/Assets/sceneswitch.cs
+++ VRGame/Assets/sceneswitch.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class sceneswitch : MonoBehaviour
+    
 {
+   
+
+
+    
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("blank");
+    }
+   
     // Start is called before the first frame update
     void Start()
     {
 
-
+       
 
     }
 
