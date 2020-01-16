c88d3c7536cd60cbd2abcbe3af16e029c608ae9f
Laura L <lmlehtinen@gmail.com>
2020-01-16 10:01:04 +0200

    äänii tasapainosceneen

---
+++ VRGame/Assets/soundtrigger2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundtrigger2 : MonoBehaviour
+{
+    public AudioClip soundtoplay;
+    public float Volume;
+    AudioSource audio;
+    public bool AlreadyPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter()
+    {
+        if (!AlreadyPlayed)
+        {
+            audio.PlayOneShot(soundtoplay, Volume);
+
+            AlreadyPlayed = true;
+        }
+    }
+    // Update is called once per frame
+
+}
