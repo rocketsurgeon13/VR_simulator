953313022030a4b01cad1276e64673d1bdfbd13b
Johanna Arponen <johanna.arponen@icloud.com>
2020-01-17 12:05:29 +0200

    Scenejen vaihto tehty

---
+++ VRGame/Assets/NextScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour{
+
+    public string loadThisScene;
+    public string rightObject;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == rightObject ) {
+            SceneManager.LoadScene(loadThisScene);
+        }
+    }
+}
