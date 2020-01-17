6ced6b8da802e82fe3445a7304b3640f2fcb7b21
Johanna Arponen <johanna.arponen@icloud.com>
2020-01-17 15:43:53 +0200

    Player checing tehty

---
+++ VRGame/Assets/PlayerChecking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class PlayerChecking : MonoBehaviour{
+
+    public GameObject playerData;
+    Player pData;
+    
+    void Start(){
+        pData = FindObjectOfType<Player>();
+        if(pData == null) {
+            pData = Instantiate(playerData).GetComponent<Player>();
+        }
+        
+    }
+
+    void Update(){
+        
+    }
+}
