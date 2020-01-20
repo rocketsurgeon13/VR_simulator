52443fd108b21d1c3eafcbdd016d8a300fea005c
Joni Halttunen <joni.halttunen@gmail.com>
2020-01-20 14:03:53 +0200

    Näön sumenemis -scenen "maalitauluun" lisätty merkkivalo pallon osuessa

--- VRGame/Assets/audiotrigger3.cs
+++ VRGame/Assets/audiotrigger3.cs
@@ -8,20 +8,33 @@ public class audiotrigger3 : MonoBehaviour
     public float Volume;
     AudioSource audio;
     public bool AlreadyPlayed = false;
+    public float lightTimer = 1f;
+    Light light;
 
     // Start is called before the first frame update
     void Start()
     {
+        light = GetComponent<Light>();
         audio = GetComponent<AudioSource>();
     }
+
     private void OnTriggerEnter()
     {
+        light.enabled = true;
         if (!AlreadyPlayed)
         {
             audio.PlayOneShot(soundtoplay, Volume);
             AlreadyPlayed = true;
         }
     }
-    // Update is called once per frame
 
+    private void Update() {
+        if (light.enabled) {
+            lightTimer -= Time.deltaTime;
+            if(lightTimer < 0) {
+                light.enabled = false;
+                lightTimer = 1f;
+            }
+        }
+    }
 }
